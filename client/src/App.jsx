dffc1396718af6c200db4f8f942927d6ca0f6414
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-03-08 23:10:31 +0530

    done upto routing

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import "./app.css"
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <div >
+      <Navbar/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+       <h1 className='logo'>SSO App</h1>
+       <ul className='list'>
+        <li className='listItem'>User Name</li>
+        <li className='listItem'>LogOut</li>
+       </ul>
+    </div>
+  )
+}
+
+export default Navbar
